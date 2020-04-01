f17b9a8a15a70d68ffed832ebcc1edd0b365b94f
Fewwy <62722417+Fewwy@users.noreply.github.com>
2020-04-01 22:01:29 +0300

    Update move.js

--- move.js
+++ move.js
@@ -1,5 +1,8 @@
 var currentScore = 0;
 var state = 0;
+var randomStart = Array(140,250,360,465,575);
+var nickname = prompt("Enter players Name");
+
 
 function animateMonster(monster){
     console.log('animateMonster ', monster);
@@ -24,11 +27,13 @@ const getDuration = function(){
 
 
  const killFunction = function(monstr) {
+    var randomPlace = randomStart[Math.floor(Math.random() * randomStart.length)];
     console.log("clicked "+ monstr.id);
     monstr.style.display = "none";
     console.log("stop animation " + monstr.id);
     $(monstr).css({marginLeft: "0px"});
     $(monstr).stop();
+    $(monstr).css({top: randomPlace + "px"});
     currentScore += 1;
     $("#currentScore").html('<b>' + currentScore + '</b>');
     console.log("show monstr " + monstr.id);
@@ -78,7 +83,7 @@ const gameover = function (){
     console.log("GAME OVER!");
     stopMonsters();
     state = 0;
-
+alert("Hráč " + nickname +  " sestřelil " + currentScore + " monster, než ho dostali.");
 }
 
 const  stopMonsters = function(){ 
@@ -92,3 +97,31 @@ function putMonstersToStart(){
         boxes[i].style.marginLeft = "0px";
     }
 } 
+
+
+function readURL(input){
+    console.log("readUrl");
+    stopMonsters();
+    var files = input.files;
+    var numMonsters = files.length;
+    console.log("num pictures "+numMonsters);
+    if (files) {
+       console.log(files);
+       var reader = new FileReader();
+       reader.onload = function(e) {
+           console.log('reader onload');
+       $(monstrimage).attr('src', e.target.result);
+      };
+    var imageInd = Math.floor(Math.random()*numMonsters);
+    reader.readAsDataURL(input.files[imageInd]);
+    console.log("image " + input.files[imageInd]);
+    }
+    else {
+        console.log('Default monster image');
+
+        //return default monster;
+        //if no files are selected, choose default monster img
+        //mImg = readImage('default_monster.png');
+    }
+
+}
